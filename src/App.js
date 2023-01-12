906cf927b728b39ae8c72472d9df9fe7e0e3b0dc
gzavlanis <gzavlanis@gmail.com>
2023-01-12 14:08:35 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Authenticator';
 import DataTable from './Components/DataTable';
-import Home from './Components/Home';
 
 export default function App(){
   return(
     <Router>
       <Routes>
-        <Route path= "/login" element = {<Login/>}/>
         <Route path= "/table" element = {<DataTable/>}/>
-        <Route path= "/" element = {<Home/>}/>
+        <Route path= "/" element = {<Login/>}/>
       </Routes>
     </Router>
   );
