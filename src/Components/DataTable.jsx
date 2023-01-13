e3782b01c8b1d57286ac4656e94c34a8286bc3a8
gzavlanis <gzavlanis@gmail.com>
2023-01-13 18:25:41 +0200

    update

--- src/Components/DataTable.jsx
+++ src/Components/DataTable.jsx
@@ -7,39 +7,44 @@ import data from "../data.json";
 
 export default function DataTable() {
     const [show, setShow] = useState(false);
+    const [sportName, setSportName] = useState("");
     const toggleModal = () => setShow(true);
     const close = () => setShow(false);
 
-    const DisplayData = data.map(
-        (info) => {
-            return(
-                <tr>
-                    <td>{info.id}</td>
-                    <td>{info.name}</td>
-                    <td style= {{width: "5%"}}>
-                        <Button onClick= {toggleModal} outline color= "secondary" type= "button" title= "Edit" className= "shadow">
-                            <FaEdit size= {18}/>
-                        </Button>
-                    </td>
-                </tr>
-            );
-        }
-    );
-
+    const handleEditClick = (e) => {
+        toggleModal();
+        data.map((sport) => {
+            if (sport.id === e.target.id){
+                setSportName(sport.name);
+            }
+        });
+    }
     return(
         <div style = {{backgroundColor: "lightgrey"}} className= "py-5">
-            <EditModal isOpen = {show} isClose = {close} toggle = {toggleModal}/>
+            <EditModal isOpen = {show} toggle = {toggleModal} isClose = {close} sport = {sportName}/>
             <Container className= "py-4 bg-white rounded shadow-lg">
                 <h4 className= "text-center">Sports</h4>
                 <Table striped size= "md">
                     <thead>
                         <tr>
-                            <th>id</th>
+                            <th>Id</th>
                             <th>Name</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {DisplayData}
+                        { data.map((sport) => {
+                            return(
+                                <tr key = {sport.id}>
+                                    <td>{sport.id}</td>
+                                    <td>{sport.name}</td>
+                                    <td style= {{width: "5%"}}>
+                                        <Button onClick= {handleEditClick} outline color= "secondary" type= "button" title= "Edit" className= "shadow" id = {sport.id}>
+                                            <FaEdit size= {18}/>
+                                        </Button>
+                                    </td>
+                                </tr>
+                            );
+                        }) }
                     </tbody>
                 </Table>
                 <NavLink to= "/" className= "btn btn-danger shadow">
--- src/Components/EditModal.jsx
+++ src/Components/EditModal.jsx
@@ -1,15 +1,22 @@
-import React from "react";
-import { Button, Modal, ModalHeader, ModalBody, Container, ModalFooter } from "reactstrap";
-import EditForm from "./EditForm";
+import React, {useState} from "react";
+import { Button, Modal, ModalHeader, ModalBody, Container, ModalFooter, Form, FormGroup, Label, Input } from "reactstrap";
 import { FaTimes } from "react-icons/fa";
 
 export default function EditModal(props){
+    const [name, setName] = useState("");
+
     return(
         <Container fluid>
-            <Modal size= "lg" isOpen = {props.isOpen}>
+            <Modal size= "md" isOpen = {props.isOpen} id = {props.id}>
                 <ModalHeader>Edit this sport</ModalHeader>
                 <ModalBody>
-                    <EditForm/>
+                    <Form>
+                        <FormGroup className= "mb-2 mx-sm-2 mb-sm-2">
+                            <Label for= "name" className= "mr-sm-2">Name</Label>
+                            <Input className= "form-control" type= "text" name= "name" id= "name" 
+                                value = {props.sport} onChange = ""/>
+                        </FormGroup>
+                    </Form>
                 </ModalBody>
                 <ModalFooter>
                     <Button color = "danger" className= "shadow" onClick= {props.isClose}>
