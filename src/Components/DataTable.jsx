3c7988de43394f49e8eb4558c12d72cc1d289606
gzavlanis <gzavlanis@gmail.com>
2023-01-16 18:16:42 +0200

    update

--- src/Components/DataTable.jsx
+++ src/Components/DataTable.jsx
@@ -1,51 +1,69 @@
 import React, {useState} from "react";
 import { Table, Container, Button } from "reactstrap";
 import { NavLink } from "react-router-dom";
-import { FaSignOutAlt, FaEdit } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
 import EditModal from "./EditModal";
 import data from "../data.json";
 
 export default function DataTable() {
     const [show, setShow] = useState(false);
     const [sportName, setSportName] = useState("");
-    const [selectedItem, setSelectedItem] = useState({});
-
     const toggleModal = () => setShow(true);
     const close = () => setShow(false);
+    const [sort, setSort] = useState(0);
+    const [sport, setSport] = useState({
+        id: '', name: ''
+    });
     
     const handleEditClick = (e) => {
-        toggleModal();
-        setSelectedItem(Object.values(data).find(sport => sport.id === e.target.id));
-        console.log(selectedItem.mame); //empty?
+        let selectedItem = data.find(sport => sport.id == e.target.id);
         setSportName(selectedItem.name);
+        toggleModal();
+    };
+    
+    const showName = (name) => {
+        console.log(name);
+        data[7].name = name;
+    };
+
+    const sortFunction = () => {
+        data.sort((a, b) => {
+            if (a.id > b.id) return 1;
+            if (a.id < b.id) return -1;
+            return 0;
+        });
+        setSort(sort + 1);
     };
 
     return(
         <div style = {{backgroundColor: "lightgrey"}} className= "py-5">
-            <EditModal isOpen = {show} toggle = {toggleModal} isClose = {close} sport = {sportName}/>
+            <EditModal isOpen = {show} toggle = {toggleModal} isClose = {close} sport = {sportName} showName = {showName}/>
             <Container className= "py-4 bg-white rounded shadow-lg">
                 <h4 className= "text-center">Sports</h4>
                 <Table striped borderless size= "md">
                     <thead>
                         <tr>
-                            <th>Id</th>
+                            <th>
+                                id&nbsp;&nbsp;
+                                <Button size = "sm" outline color = "secondary" className= "shadow py-1" type= "button" title= "Sort by ID" onClick= {sortFunction}>
+                                    <i className= "fa fa-sort fa-lg"></i>
+                                </Button>
+                            </th>
                             <th>Name</th>
                         </tr>
                     </thead>
                     <tbody>
-                        { data.map((sport) => {
-                            return(
-                                <tr key = {sport.id}>
-                                    <td>{sport.id}</td>
-                                    <td>{sport.name}</td>
-                                    <td style= {{width: "5%"}}>
-                                        <Button onClick= {handleEditClick} outline color= "secondary" type= "button" title= "Edit" className= "shadow" id = {sport.id}>
-                                            <FaEdit size= {18}/>
-                                        </Button>
-                                    </td>
-                                </tr>
-                            );
-                        }) }
+                        { data.map(sport => (
+                            <tr key = {sport.id}>
+                                <td>{sport.id}</td>
+                                <td>{sport.name}</td>
+                                <td style= {{width: "5%"}}>
+                                    <Button onClick= {handleEditClick} outline color= "secondary" type= "button" title= "Edit" className= "shadow px-2 py-2" id = {sport.id}>
+                                        <i className= "fa fa-edit fa-lg" id = {sport.id}></i>
+                                    </Button>
+                                </td>
+                            </tr>))
+                        }
                     </tbody>
                 </Table>
                 <NavLink to= "/" className= "btn btn-danger shadow">
--- src/Components/EditModal.jsx
+++ src/Components/EditModal.jsx
@@ -1,27 +1,32 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Button, Modal, ModalHeader, ModalBody, Container, ModalFooter, Form, FormGroup, Label, Input } from "reactstrap";
 import { FaTimes } from "react-icons/fa";
 
-export default function EditModal(props){
-    const [name, setName] = useState("");
+export default function EditModal({sport, isOpen, isClose, showName}){
+    const [name, setName] = useState(sport);
     const handleChange = (e) => {
         setName(e.target.value);
+        showName(e.target.value);
     };
+
+    useEffect(() => {
+        setName(sport);
+    }, [sport]);
 
     return(
         <Container fluid>
-            <Modal size= "md" isOpen = {props.isOpen} id = {props.id}>
-                <ModalHeader>Edit this sport</ModalHeader>
+            <Modal size= "md" isOpen = {isOpen}>
+                <ModalHeader>Edit {name}</ModalHeader>
                 <ModalBody>
                     <Form>
                         <FormGroup className= "mb-2 mx-sm-2 mb-sm-2">
                             <Label for= "name" className= "mr-sm-2">Name</Label>
-                            <Input type= "text" name= "name" id= "name" value = {props.sport} onChange = {handleChange}/>
+                            <Input type= "text" name= "name" id= "name" value = {name} onChange = {handleChange}/>
                         </FormGroup>
                     </Form>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color = "danger" className= "shadow" onClick= {props.isClose}>
+                    <Button color = "danger" className= "shadow" onClick= {isClose}>
                         <FaTimes size= {18}/> Cancel
                     </Button>
                 </ModalFooter>
